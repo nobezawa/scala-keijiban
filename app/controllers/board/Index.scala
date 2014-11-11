604c510abd277ae24d25b12a22c592b91b7a217d
nobezawa <charmy.nobezawa@gmail.com>
2014-11-11 21:40:13 +0900

    ファイルの追加

---
+++ app/controllers/board/Index.scala
@@ -0,0 +1,11 @@
+package controllers.board
+
+import play.api._
+import play.api.mvc._
+import play.api.data.Form
+import play.api.data.Forms._
+import scala.slick.model.codegen.SourceCodeGenerator
+
+Object Index extends Controller{
+
+}
